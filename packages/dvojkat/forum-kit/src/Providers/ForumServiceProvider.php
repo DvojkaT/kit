b361ee167e010fcb325d9da0ccbab4a92e42280f
DvojkaT <romannskv02@gmail.com>
2023-07-08 20:10:29 +0700

    feat(forum-kit): Добавлен и подключен локальный пакет forum-kit

---
+++ packages/dvojkat/forum-kit/src/Providers/ForumServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace DvojkaT\Forumkit\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ForumServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+
+    }
+}
